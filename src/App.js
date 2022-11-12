e8da081d6cdbd6f9652bf16d531418907add7f87
BERNALD BINISH BABY <bernald@BERNALDs-iMac.local>
2022-11-12 11:02:38 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+    const[addtext ,setAddtext] = useState([]);
+    const[inputtext,setInputtext] = useState ("");
+    const[edittext,setEdittext] =useState("");
+    const[changevalue ,setChangevalue] =useState("")
+
+    
+  const addbutton=()=>{
+
+   console.log("hai button");
+   setAddtext([...addtext ,{id : Date.now () , name :inputtext}])
+
+  }
+   
+
+  const changetext =(h1value) => {
+    console.log(h1value)
+    setEdittext("");
+
+    const newValue= addtext.map((obj)=>{
+    
+      if(obj.id===h1value.id){
+
+        return {...obj , name : changevalue }
+         
+
+      }
+       
+
+      return obj;
+
+    })
+   
+     setAddtext(newValue);
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     
+     <div className='todoapp'>
+          <div className='h1todo'>
+            <h1 className='h1text'>TODO APP</h1>
+           </div>
+        
+         <input className='input-main' type="text" placeholder='add content' onChange={(e)=>setInputtext(e.target.value)}  ></input>
+         <button className='addbtn' onClick={()=>addbutton()}>ADD</button>
+
+     </div>
+  
+     <div>
+    
+    {addtext.map((h1text ,index)=>{
+
+      return(
+ 
+      <div className='todolist' key={index} >
+
+
+
+        {edittext===index ? (<div><input className='inputnew' onChange={(e)=>setChangevalue(e.target.value)}></input></div>) : (<div> <h1 className='todotext'>{h1text.name}</h1></div>)}
+
+        {edittext=== index  ? (<button className='editbtn' onClick={()=>changetext(h1text)}>OK</button>):<button className='editbtn' onClick={()=>setEdittext(index)}>EDIT</button>}
+       
+        <button className='delete' onClick={()=>setAddtext(addtext.filter ((obj)=> obj.id !== h1text.id )) }>X</button>
+        
+        
+        
+
+       </div>    
+
+
+
+  
+
+      )})}
+
+       
+   </div>
+      
+       
+
+
+
+     
     </div>
   );
 }
